ca533dd67b3a9abcbd2e0d46d2c5ac3ab7f66ae9
maz <maziar.mir.2002@gmail.com>
2026-09-15 11:39:08 +0330

    improved template implementation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
 	"net/http"
 	"strconv"
 
@@ -22,23 +21,25 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 
+	// data := &templateData{Snippets: s}
+	//
 	// files := []string{
 	// 	"./ui/html/home.page.tmpl",
 	// 	"./ui/html/base.layout.tmpl",
 	// 	"./ui/html/footer.partial.tmpl",
 	// }
-
+	//
 	// ts, err := template.ParseFiles(files...)
 	// if err != nil {
 	// 	app.serverError(w, err)
 	// 	return
 	// }
-
-	// err = ts.Execute(w, nil)
+	//
+	// err = ts.Execute(w, data)
 	// if err != nil {
 	// 	app.serverError(w, err)
 	// }
@@ -61,7 +62,28 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
+
+	// data := &templateData{Snippet: s}
+
+	// files := []string{
+	// 	"./ui/html/show.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, data)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
